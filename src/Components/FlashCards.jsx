01ef890b8dfcfb9a658c14a070da642579f32f03
Thomson Cheung <thomsoncheung223@gmail.com>
2023-03-07 22:21:34 -0500

    Second version with css of cards

--- src/Components/FlashCards.jsx
+++ src/Components/FlashCards.jsx
@@ -3,9 +3,18 @@ import { useState } from "react";
 
 const FlashCards = (props) => {
     const [flip, setFlip] = useState(true);
+    
     return(
-        <div onClick={() => setFlip(!flip)} className='card'>
-            {flip ? props.question : props.answer}
+        <div
+         onClick={() => setFlip(!flip)} 
+         className={`card ${flip ? '' : 'flip'}`}
+         >
+            <div className="front">
+                {props.question}
+            </div>
+            <div className="back">
+                {props.answer}
+            </div>
         </div>
     )
 }
